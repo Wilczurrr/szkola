87758558ae4ab6b8b82d36746b1670a9c17d55cf
Wilczurrr <145229025+Wilczurrr@users.noreply.github.com>
2023-09-27 14:10:19 +0200

    Update index.js

--- Liczenie_Pola/index.js
+++ Liczenie_Pola/index.js
@@ -9,7 +9,7 @@ function licz() {
     let obw = parseFloat(2*a+2*b)
     let pole = parseFloat(a*b)
     let przek = parseFloat(Math.sqrt(a**2 + b**2)).toFixed(3)
-    
+
     document.getElementById("pole").value = pole
     document.getElementById("obwod").value = obw
     document.getElementById("przek").value = przek
@@ -27,12 +27,41 @@ function licz() {
 
 
 
-
-
-
-
-
-
-
-
 }
+
+
+
+//ARRAY
+
+const lista = [1,232,64,123,7,3,1]
+
+let text = ""
+
+for(let i = 0; i < lista.length ;i++){
+
+    text += lista[i] + "<br>";
+}
+
+document.getElementById("przed").innerHTML = text
+
+const newlista = lista.sort(function(a, b){return a - b})
+text = ""
+
+for(i = 0; i < newlista.length; i++){
+
+    text += "<li>" + newlista[i];
+}
+
+document.getElementById("po").innerHTML = text
+
+
+const randomlista = lista.sort(function(){return 0.5 - Math.random()})
+text = ""
+
+
+for(i = 0; i < randomlista.length; i++){
+
+    text += randomlista[i] + "<br>";
+}
+
+document.getElementById("random").innerHTML = text
